c3cadb0aeac7bb5d603b1e6d0833b4588d736af4
Andrew Kornilov <frutik@gmail.com>
2011-06-02 15:57:49 +0300

    added testing asterisk connector

---
+++ utils/asterisk-connector-mock/handlers/commands.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+def handle_Dial(raw):
+    return raw
+
+def handle_Hangup(raw):
+    return raw
+
+def handle_Link(raw):
+    return raw
+
+def handle_Newcallerid(raw):
+    return raw
+
+def handle_Newchannel(raw):
+    return raw
+
+def handle_Newexten(raw):
+    return raw
+
+def handle_Newstate(raw):
+    return raw
+
+def handle_Unlink(raw):
+    return raw
+
+
