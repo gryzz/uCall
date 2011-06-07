7079d79f333ddd23286e443ca2a4f1ed1f508593
Andrew Kornilov <frutik@gmail.com>
2011-06-07 20:03:24 +0300

    - refactored main template - added inheritance into login template

--- web/configurator/urls.py
+++ web/configurator/urls.py
@@ -6,5 +6,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', 'ucall_ui.views.main'),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout_then_login'),
     url(r'^admin/', include(admin.site.urls)),
 )
