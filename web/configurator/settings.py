3cbc4321a4d14ce532b8af8a80a6942ce10ce5d8
rstets <romko.stets@gmail.com>
2011-06-07 21:10:43 +0300

    - Added a comma

--- web/configurator/settings.py
+++ web/configurator/settings.py
@@ -103,7 +103,8 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'configurator.urls'
 
 TEMPLATE_DIRS = (
-    '/opt/ucall/web/configurator/templates'
+    '/opt/ucall/web/configurator/templates',
+    
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
