f352a61f197152a184db5ce2526a817b56c5a3ec
Andrew Kornilov <frutik@gmail.com>
2011-06-07 17:14:21 +0300

    added templates

--- web/configurator/ucall_ui/views.py
+++ web/configurator/ucall_ui/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 
+@login_required()
 def main(request):
     return render_to_response('ucall_ui/main.html', {'agent': 'SIP/1113', 'username': 'Andrew Kornilov'})
