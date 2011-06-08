cc0ccc45a87d2bdb3ac5482a22a09baf995a6b45
Andrew Kornilov <frutik@gmail.com>
2011-06-08 12:16:20 +0300

    added use of url back resolv

--- web/configurator/urls.py
+++ web/configurator/urls.py
@@ -4,9 +4,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'ucall_ui.views.main'),
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
-    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout_then_login'),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^profile/', 'ucall.views.main'),
+    url(r'^$', 'ucall_ui.views.main', name="root"),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login', name="login"),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout_then_login', name="logout"),
+    url(r'^admin/', include(admin.site.urls), name="admin"),
+    url(r'^profile/', 'ucall.views.main', name="profile"),
 )
