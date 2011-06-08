4477820926471ea08cdeb6140b1cf04a29a4c535
rstets <romko.stets@gmail.com>
2011-06-08 05:13:57 +0300

    - Added vendor dir with extjs 4

--- web/ui/app.js
+++ web/ui/app.js
@@ -1,4 +1,17 @@
-/**
- * Bootstrap
- */
+Ext.application({
+    name: 'uCall',
+    appFolder: 'app',
 
+    launch: function() {
+        Ext.create('Ext.container.Viewport', {
+            layout: 'fit',
+            items: [
+                {
+                    xtype: 'panel',
+                    title: 'uCall',
+                    html : 'Application will go here'
+                }
+            ]
+        });
+    }
+});
