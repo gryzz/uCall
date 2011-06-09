465daaab15c3bce6c89fa8eea43216d516113bb9
Ivan Yarych <yarych@gmail.com>
2011-06-09 16:18:36 +0300

    app.js -> bootstrap.js

---
+++ web/ui/bootstrap.js
@@ -0,0 +1,19 @@
+// Configuration
+Ext.Loader.setConfig({enabled: true});
+Ext.Loader.setPath('uCall', 'ui/app/uCall');
+
+// Includes
+Ext.require('uCall.App');
+
+// Application
+Ext.application({
+    name: 'uCall',
+    launch: function() {
+        Ext.create('uCall.App');
+    }
+});
+
+// Ready
+Ext.onReady(function(){
+	// TODO: Do something
+});
