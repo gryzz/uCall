249c275d5a9ca0389ca40b023c1cbaf7488701c2
ikulmatytskyy <ihorko@mail.ua>
2011-06-09 15:01:51 +0300

    - Add basic UI to main.html

--- web/ui/app.js
+++ web/ui/app.js
@@ -1,6 +1,6 @@
 // Configuration
 Ext.Loader.setConfig({enabled: true});
-Ext.Loader.setPath('uCall', 'app/uCall');
+Ext.Loader.setPath('uCall', 'ui/app/uCall');
 
 // Includes
 Ext.require('uCall.App');
