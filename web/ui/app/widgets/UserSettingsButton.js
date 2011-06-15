72b40a4f95e6d4bb3e4bec7f0494f67253527d7a
Igorko <gryzzbox@gmail.com>
2011-06-16 00:57:24 +0300

    Tuning user profile

--- web/ui/app/widgets/UserSettingsButton.js
+++ web/ui/app/widgets/UserSettingsButton.js
@@ -17,55 +17,13 @@ Ext.define('uCall.widgets.UserSettingsButton', {
     onClick: function(){
 		Ext.create("uCall.widgets.UserSettingsWindow");
 	},
-	
-	onChannelStatusActive: function() {
-		// TODO: Implement
-	},
-	
-	onChannelStatusInactive: function() {
-		// TODO: Implement
-	},
-	
     config: {
     	id: 'ChannelStatusIndicator',
-		handler: this.onClick
+	handler: this.onClick
     },
     
 	constructor: function(){
 		Ext.applyIf(this, this.config);
 		this.callParent(arguments);
-		this.setIcon(this.inactiveStatusImageSrc);
-		
-		console.log("TODO: ChannelStatusIndicator(). Pass a provider!");
-		console.log("TODO: ChannelStatusIndicator(). Create 'real' events and add listeners!");
-	},
-	
-	showPopup: function() {
-		Ext.create("uCall.widgets.ChannelStatusInactivePopup").show();
-	},
-	
-	delayedFakeDisconnect: function() {
-		console.log("TODO: ChannelStatusIndicator.delayedFakeDisconnect. Remove!");
-		
-		setTimeout(function(that) {
-			that.channelStatusActive = false;
-			that.setIcon(that.inactiveStatusImageSrc);
-			
-			that.showPopup();
-		}, 5000, this);		
-
-	},
-	
-	channelReconnect: function() {
-		console.log("TODO: ChannelStatusIndicator.channelReconnect. Bind to provider!");
-		this.channelStatusActive = true;
-		this.setIcon(this.activeStatusImageSrc);
-		this.delayedFakeDisconnect();
-	},
-	
-	isChannelStatusActive: function() {
-		console.log("TODO: ChannelStatusIndicator.isChannelStatusActive. Bind to provider!");
-		return this.channelStatusActive;
-	}
-    
+	}    
 });
