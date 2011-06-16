ee5a62f4c1b6d5cddcbd1214fd35b4d254b84bc4
Igorko <gryzzbox@gmail.com>
2011-06-16 04:48:39 +0300

    Added preventional double user settings window creation.

--- web/ui/app/widgets/UserSettingsButton.js
+++ web/ui/app/widgets/UserSettingsButton.js
@@ -9,17 +9,21 @@ Ext.define('uCall.widgets.UserSettingsButton', {
 	requires: "uCall.widgets.UserSettingsWindow",
     extend: 'Ext.Button',
     alias: 'widget.UserSettingsButton',
-    
+
     iconSrc: "/ui/resources/images/user_settings/gear.png",
-    
+
     onClick: function(){
-		Ext.create("uCall.widgets.UserSettingsWindow");
+        var userSettingsWindow = Ext.getCmp("UserSettingsWindow");
+        if (userSettingsWindow == undefined) {
+            userSettingsWindow = Ext.create("uCall.widgets.UserSettingsWindow");
+        }
+		userSettingsWindow.show();
 	},
     config: {
     	id: 'UserSettingsButton',
 		handler: this.onClick
     },
-    
+
 	constructor: function(){
 		Ext.applyIf(this, this.config);
 		this.callParent(arguments);
