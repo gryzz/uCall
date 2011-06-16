37926aa35d49b354e77ae0a0820d504091f31b97
rstets <romko.stets@gmail.com>
2011-06-16 14:39:56 +0300

    - Channel status indication (No reconnect yet)

--- web/ui/app/App.js
+++ web/ui/app/App.js
@@ -7,8 +7,10 @@
 
 Ext.define('uCall.App', {
     requires: [
-	'uCall.widgets.MainPanel',
-	'uCall.controllers.MessagesDeck'
+		'uCall.widgets.MainPanel',
+		'uCall.controllers.MessagesDeck',
+		'uCall.data.stomp.StompClientAdapterFactory',
+		'uCall.controllers.ChannelEventController'
     ],
     
     extend: 'Ext.container.Viewport',
@@ -23,12 +25,74 @@ Ext.define('uCall.App', {
         	}
         ]
     },
+    // Declare members
+    messageDeck: null,
+	stompClientAdapterFactory: null,
+	stompClientAdapter: null,
+	channelEventController: null,
     
+    // Constructor
 	constructor: function(){
+		var that = this;
+		
+		// Copy config values to current object
 		Ext.applyIf(this, this.config);
+		// Call parent constructor
 		this.callParent(arguments);
-		console.log("TODO: App(). Create 'User' model and keep user's data inside it.");
 
+        /*
+         * Init app components
+         */
+         
+        // Message deck
 		this.messageDeck = Ext.create('uCall.controllers.MessagesDeck');
+		
+		// performChannelReconnect
+		this.channelStatusIndicator = Ext.getCmp("ChannelStatusIndicator");
+
+		// Create channel event controller		
+		this.channelEventController = Ext.create("uCall.controllers.ChannelEventController", {
+			onConnect: function(){
+				that.channelStatusIndicator.fireEvent(uCall.constants.ChannelEvent.CONNECTED);
+			},
+			onMessage: function(){
+				console.log("onMessage");
+			},
+			onDisconnect: function(){
+				that.channelStatusIndicator.fireEvent(uCall.constants.ChannelEvent.DISCONNECTED);
+			},
+		});
+
+		
+        // Stomp client adapter factory
+		this.stompClientAdapterFactory = Ext.create('uCall.data.stomp.StompClientAdapterFactory', {
+			url: 'ws://rstets.ucserv.dev:61614/stomp',
+			login: 'guest',
+			passcode: 'password',
+			destination: '/queue/messages/SIP/1001',
+			sendDestination: '/queue/control',
+			pingMessage: 'SIP/1001',
+			debug: true, 
+			
+			onConnectCallback: function(){
+				// Propagate channel connect event
+				that.channelEventController.fireEvent(uCall.constants.ChannelEvent.CONNECTED);
+			},
+			onDisconnectCallback: function(){
+				// Propagate channel disconnect event
+				that.channelEventController.fireEvent(uCall.constants.ChannelEvent.DISCONNECTED);
+			},
+			onMessageCallback: function(data){
+				// Propagate channel message event
+				that.channelEventController.fireEvent(uCall.constants.ChannelEvent.MESSAGE, {message: data});
+			}
+		});
+		
+        // Stomp client adapter
+		this.stompClientAdapter = this.stompClientAdapterFactory.getAdapter();
+		
+		// Do connect
+		this.stompClientAdapter.performConnect();
+		
 	}    
 });
