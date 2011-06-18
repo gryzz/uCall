026ac38677efda313553a1ce37ceca5e50826b1f
rstets <romko.stets@gmail.com>
2011-06-18 04:06:54 +0300

    - Formunculous

--- web/ucall_backend/urls.py
+++ web/ucall_backend/urls.py
@@ -1,6 +1,12 @@
+import os
+import sys
+
 from django.conf.urls.defaults import patterns, include, url
 from ucall.rpc import Router
 from django.contrib import admin
+
+PROJECT_ROOT = os.path.dirname(__file__)
+FORMUNCULOUS_STATIC_MEDIA_PATH = os.path.join(PROJECT_ROOT, "vendors", "formunculous", "formunculous", "media", "formunculous")
 
 admin.autodiscover()
 router = Router()
@@ -19,5 +25,7 @@ urlpatterns = patterns('',
     url(r'^config/urls/', 'ucall.views.config_urls', name="config_urls"),
     
     # include formunculous urls
-    (r'^func/', include('formunculous.urls')),
+    (r'^admin/forms/', include('formunculous.urls')),
+    # formunculous static urls
+    (r'formunculous/(?P<path>.*)$', 'django.views.static.serve', {'document_root': FORMUNCULOUS_STATIC_MEDIA_PATH, 'show_indexes': False}),    
 )
