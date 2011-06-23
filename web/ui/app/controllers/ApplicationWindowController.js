2bf97e716d1a6d8ed48f8274f6116f8959f4cd07
rstets <romko.stets@gmail.com>
2011-06-23 14:08:15 +0300

    - Show dialog window on clearing event

--- web/ui/app/controllers/ApplicationWindowController.js
+++ web/ui/app/controllers/ApplicationWindowController.js
@@ -1,47 +1,86 @@
 /**
- * @class uCall.controllers.MessageController
+ * @class uCall.controllers.ApplicationWindowController
  * @extends Ext.util.Observable
  *
- * Message controller.
+ * Application Window controller.
  */
 
 Ext.define('uCall.controllers.ApplicationWindowController', {
-    requires: ['uCall.constants.MessageEvent'],
+    requires: [
+        'uCall.constants.MessageEvent',
+        'uCall.model.ApplicationDefinition',
+        'uCall.widgets.ApplicationFormWindow'
+    ],
+    
     extend: 'Ext.util.Observable',
     
-    mappedEvents: channelEventSchema,
-    
-    handle: function(m) {
-        var message = Ext.JSON.decode(m.message.data.body);
+    onShow: function(message){
+	    console.log("ApplicationWindowController::onShow");
+	    console.log("TODO: pass the message to RPC router");
 
-        switch(message.t){
-            case this.mappedEvents.EVENT_RINGING:
-                this.fireEvent(uCall.constants.MessageEvent.SHOW, message.u, 'User ' + message.u + ' is waiting ...');
-            break;
-            case this.mappedEvents.EVENT_HANGUP_CLEANUP:
-                this.fireEvent(uCall.constants.MessageEvent.HIDE, message.u);
-            break;
-            case this.mappedEvents.EVENT_LINK:
-                this.fireEvent(uCall.constants.MessageEvent.WINDOW, message.u);
-            break;
-        }
-    },
-    
-    onShow: function(m){
-	alert('!!!!');
-    }
-    
-    config: {
-        onShow: Ext.emptyFn,
-        onHide: Ext.emptyFn
+        uCall.model.ApplicationDefinition.load(1, {
+            success: function(result) {
+        
+                var applicationDefinition = result.data;
+                var applicationFormItems = applicationDefinition.application_form;
+                applicationFormItems.push({
+                    xtype: 'hiddenfield',
+                    name: 'application_definition_id',
+                    value: applicationDefinition.id
+                });
+        
+                // TODO: Do something with this!
+                for (var item in applicationFormItems) {
+                    applicationFormItems[item].labelWidth = 200;
+                    applicationFormItems[item].labelAlign = "right";
+                }
+        
+                Ext.create('uCall.widgets.ApplicationFormWindow', {
+                    title: applicationDefinition.slug, // TODO: use app.def. title!
+                    formItems: applicationFormItems,
+                    onSubmit: function() {
+                        // TODO: Implement! (Copy-Paste from update profile)
+        
+                     var form = this.up('form').getForm();
+                        if (form.isValid()) {
+                            that = this;
+                            form.submit({
+                                waitMsg: 'Submitting your data...',
+                                success: function(form, action){
+                                    //TODO: Add messageBox that exdent common one
+                                    that.up('form').getForm().reset();
+                                    that.up('window').hide();
+                                    Ext.MessageBox.alert('Thank you!', 'Your application has been saved.');
+                                },
+                                failure: function(form, action){
+                                     var messageBox = Ext.create('uCall.widgets.GrowlMessage', {
+                                        items: {
+                                            xtype: 'component',
+                                            html: 'Something went wrong',
+                                        },
+                                        floating: true,
+                                        closable: true,
+                                        id: 'id'}
+                                     );
+        
+                                     messageBox.show();
+                                }
+                            });
+                     }
+                    }
+                });
+            }
+        });
+
     },
     
     constructor: function(config) {
         // Parent
         this.callParent(arguments);
+        
         // Merge configs
         Ext.apply(this.config, config);
-        Ext.applyIf(this, this.config);
+        Ext.apply(this, this.config);
 
         this.on(uCall.constants.MessageEvent.WINDOW, this.onShow, this);
     }
--- web/ui/app/controllers/MessageController.js
+++ web/ui/app/controllers/MessageController.js
@@ -11,38 +11,45 @@ Ext.define('uCall.controllers.MessageController', {
     
     mappedEvents: channelEventSchema,
     
-    handle: function(m) {
-        var message = Ext.JSON.decode(m.message.data.body);
-
-        switch(message.t){
-            case this.mappedEvents.EVENT_RINGING:
-                this.fireEvent(uCall.constants.MessageEvent.SHOW, message.u, 'User ' + message.u + ' is waiting ...');
-            break;
-            case this.mappedEvents.EVENT_HANGUP_CLEANUP:
-                this.fireEvent(uCall.constants.MessageEvent.HIDE, message.u);
-            break;
-            case this.mappedEvents.EVENT_LINK:
-                this.fireEvent(uCall.constants.MessageEvent.HIDE, message.u);
-            break;
-        }
-    },
-    
     config: {
+        onEventLinkCallback: Ext.emptyFn,
+        
         onShow: Ext.emptyFn,
         onHide: Ext.emptyFn
     },
     
+    handleMessage: function(eventData) {
+        var message = Ext.JSON.decode(eventData.message.data.body);
+        
+        switch(message.t){
+            case this.mappedEvents.EVENT_RINGING:
+                this.fireEvent(uCall.constants.MessageEvent.SHOW, message.u, 'User ' + message.u + ' is waiting ...');
+                break;
+            case this.mappedEvents.EVENT_HANGUP_CLEANUP:
+            //     this.fireEvent(uCall.constants.MessageEvent.HIDE, message.u);
+            //     break;
+            // case this.mappedEvents.EVENT_LINK:
+                // this.fireEvent(uCall.constants.MessageEvent.HIDE, message.u);
+                
+                if (this.onEventLinkCallback) {
+                    this.onEventLinkCallback(message);
+                }
+                break;
+        }
+    },
+    
     constructor: function(config) {
-        // Parent
-        this.callParent(arguments);
         // Merge configs
         Ext.apply(this.config, config);
-        Ext.applyIf(this, this.config);
+        Ext.apply(this, this.config);
+        
+        // Parent
+        this.callParent(arguments);
+        
         // Register events
         this.addEvents(
             uCall.constants.MessageEvent.SHOW,
-            uCall.constants.MessageEvent.HIDE,
-            uCall.constants.MessageEvent.WINDOW
+            uCall.constants.MessageEvent.HIDE
         );
         // Add listeners
         this.on(uCall.constants.MessageEvent.SHOW, this.onShow, this);
