587b8274144e64204b8c0b2dc8e59b89f1203c25
Andrew Kornilov <frutik@gmail.com>
2011-06-27 17:25:25 +0300

    - testing and production pbx connector merged

--- utils/asterisk-connector/ami2stomp.py
+++ utils/asterisk-connector/ami2stomp.py
@@ -8,20 +8,23 @@ import simplejson as json
 from stompy.simple import Client
 import ConfigParser
 from sqlobject import *
-from amievent import AsteriskEventDial
+import handlers
 
-#import fcntl                                                                                                                                                                                    
-#lockfile = os.path.normpath('/var/lock/' + os.path.basename(__file__) + '.lock')                                                                                                                
-#exclusive_lock = open(lockfile, 'w')                                                                                                                                                            
-#try:                                                                                                                                                                                            
-#    fcntl.lockf(exclusive_lock, fcntl.LOCK_EX | fcntl.LOCK_NB)                                                                                                                                  
-#except IOError:                                                                                                                                                                                 
-#    print "Another instance is already running, quitting."                                                                                                                                      
-#    time.sleep(1)                                                                                                                                                                               
-#    sys.exit(-1)    
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+from channel.channel_message import ChannelMessage as ChannelMessage
+
+import fcntl                                                                                                                                                                                    
+lockfile = os.path.normpath('/var/lock/' + os.path.basename(__file__) + '.lock')                                                                                                                
+exclusive_lock = open(lockfile, 'w')                                                                                                                                                            
+try:                                                                                                                                                                                            
+    fcntl.lockf(exclusive_lock, fcntl.LOCK_EX | fcntl.LOCK_NB)                                                                                                                                  
+except IOError:                                                                                                                                                                                 
+    print "Another instance is already running, quitting."                                                                                                                                      
+    time.sleep(1)                                                                                                                                                                               
+    sys.exit(-1)    
 
 config = ConfigParser.ConfigParser()
-config.read('/opt/etc/asterisk/config.ini')
+config.read('/opt/ucall/etc/config.ini')
 
 stomp = Client(config.get('STOMP', 'host'))
 stomp.connect(config.get('STOMP', 'username'), config.get('STOMP', 'password'))
@@ -39,105 +42,17 @@ connection = connectionForURI(config.get('SQL', 'dsn'))
 sqlhub.processConnection = connection
 
 def send_message(message, agent):
-    conf={"expires":(int(time()) + int(connect(config.get('GENERAL', 'message_ttl'))) * 1000}  
+    #conf={"expires":(int(time()) + int(connect(config.get('GENERAL', 'message_ttl'))) * 1000}  
     stomp.put(message, destination="/queue/messages/"+agent, persistent=False, conf=conf)
-
-def getLocalNumber(channel):
-    return channel.split('-')[0]
 
 def handle_shutdown(event, manager):
     print "Recieved shutdown event"
     manager.close()
     # we could analize the event and reconnect here
-      
-def handle_newstate_ringing(event, manager):
-    channel = event.get_header('Channel')
-    
-    if channel == None:
-        return False
-
-    initiator = AsteriskEventDial.selectBy(destuniqueid=event.get_header('Uniqueid'))
-    initiator = initiator[0]
-	
-    agent = getLocalNumber(channel)
-    caller = initiator.callerid
-    ext = event.get_header('CallerID')
-    uid = event.get_header('Uniqueid')
-
-    message = json.dumps({'event': 'ringing', 'ext':ext, 'agent':agent, 'uid':uid, 'caller':caller}, separators=(',',':'))
-    
-    send_message(message, agent)
-    #stomp.put(message, destination="/queue/messages/"+agent, persistent=False)
-    print "Incomming ringing - %s. %s -> %s -> %s" % (uid, caller, ext, agent)
-      
-      
-def handle_newstate(event, manager):
-    #print event.get_header('Event'), event.headers
-    
-    if event.get_header('State') == 'Ringing':
-	handle_newstate_ringing(event, manager)
-	
-#def handle_newstate(event, manager):
-#    #print event.get_header('Event'), event.headers
-#    if event.get_header('State') == 'Ringing':
-#	channel2 = event.get_header('Channel2')
-#    
-#	if channel2 == None:
-#	    return False
-#    
-#    agent = getLocalNumber(channel2)
-#    client = event.get_header('CallerID1')
-#    
-#    if event.get_header('State') == 'Ringing':
-#	message = json.dumps({'event': 'link', 'remote':client, 'local':agent}, separators=(',',':'))
-#	stomp.put(message, destination="/queue/"+agent)
-#	print "Incomming ringing - %s - %s - %s" % (event.get_header('Uniqueid1'), client, agent)
-
-def handle_hangup(event, manager):
-    #print event.get_header('Event'), event.headers
-    
-    if event.get_header('Cause-txt') == 'Normal Clearing':
-	handle_hangup_clearing(event, manager)
-
-def handle_hangup_clearing(event, manager):
-    channel = event.get_header('Channel')
-    
-    if channel == None:
-        return False
-    
-    agent = getLocalNumber(channel)
-    uid = event.get_header('Uniqueid')
-    
-    message = json.dumps({'event': 'hangup_cleanup', 'agent':agent, 'uid':uid}, separators=(',',':'))
-    stomp.put(message, destination="/queue/messages/"+agent, persistent=False)
-    print "Hangup - %s - %s" % (uid, agent)
-
-def handle_link(event, manager):
-    agent = getLocalNumber(event.get_header('Channel2'))
-    message = json.dumps({'event': 'link', 'remote': event.get_header('CallerID1'), 'local':agent}, separators=(',',':'))
-
-    stomp.put(message, destination="/queue/"+agent, persistent=False)
-    print "Incomming answered - %s - %s - %s" % (event.get_header('Uniqueid1'), event.get_header('CallerID1'), getLocalNumber(event.get_header('Channel2')))
-	
-def handle_unlink(event, manager):
-    agent = getLocalNumber(event.get_header('Channel2'))
-    message = json.dumps({'event': 'unlink', 'remote': event.get_header('CallerID1'), 'local':agent}, separators=(',',':'))
-    stomp.put(message, destination="/queue/messages/"+agent, persistent=False)
-    print "Incomming terminated - %s - %s - %s" % (event.get_header('Uniqueid1'), event.get_header('CallerID1'), getLocalNumber(event.get_header('Channel2')))
-
-def handle_dial(event, manager):
-    #try:
-    srcuniqueid = event.get_header('SrcUniqueID')
-    callerid = event.get_header('CallerID')
-    destination = getLocalNumber(event.get_header('Destination'))
-    destuniqueid = event.get_header('DestUniqueID')
-    source = getLocalNumber(event.get_header('Source'))
-
-    AsteriskEventDial(srcuniqueid=srcuniqueid, callerid=callerid, destination=destination, destuniqueid=destuniqueid, source=source)
-    #except:
-    #	pass
 
 def handle_event(event, manager):
+    return 
+    
     try:
 	srcuniqueid=event.get_header('Uniqueid')
     except:
@@ -149,41 +64,36 @@ def handle_event(event, manager):
 
 manager = asterisk.manager.Manager()
 
-try:
-    # connect to the manager
-    try:
-	manager.connect(config.get('AMI', 'host')) 
-        manager.login(config.get('AMI', 'username'), config.get('AMI', 'password'))
+#try:
+#try:
+manager.connect(config.get('AMI', 'host'))
+manager.login(config.get('AMI', 'username'), config.get('AMI', 'password'))
 
-        manager.register_event('Shutdown', handle_shutdown)
-        manager.register_event('Newstate', handle_newstate)
-        manager.register_event('Hangup', handle_hangup)
-        manager.register_event('Link', handle_link)
-        manager.register_event('Unlink', handle_unlink)
-        manager.register_event('Dial', handle_dial)
-        #manager.register_event('Newstate', handle_newstate)
-        #manager.register_event('*', handle_event)
+manager.register_event('Shutdown', handle_shutdown)
+manager.register_event('Newstate', handlers.handle_Newstate)
+manager.register_event('Hangup', handlers.handle_Hangup)
+manager.register_event('Link', handlers.handle_Link)
+manager.register_event('Unlink', handlers.handle_Unlink)
+manager.register_event('Dial', handlers.handle_Dial)
+manager.register_event('Newstate', handlers.handle_Newstate)
+manager.register_event('*', handle_event)
 
-#        while True: # get a status report
-#    	    response = manager.status()
-#	    time.sleep(0.1)
-	    
-#	    sys.stdout.flush()
-#	    sys.stderr.flush()
+manager.message_loop()
 
-	manager.message_loop()
+manager.logoff()
 
-        manager.logoff()
-    except asterisk.manager.ManagerSocketException, (errno, reason):
-	print "Error connecting to the manager: %s" % reason
-        sys.exit(1)
-    except asterisk.manager.ManagerAuthException, reason:
-	print "Error logging in to the manager: %s" % reason
-        sys.exit(1)
-    except asterisk.manager.ManagerException, reason:
-	print "Error: %s" % reason
-        sys.exit(1)
-          
-finally:
-    # remember to clean up
-    manager.close()
+#except asterisk.manager.ManagerSocketException, (errno, reason):
+#    print "Error connecting to the manager: %s" % reason
+
+#except asterisk.manager.ManagerAuthException, reason:
+#    print "Error logging in to the manager: %s" % reason
+
+#except asterisk.manager.ManagerException, reason:
+#    print "Error: %s" % reason
+
+#except:
+#    sys.exit()
+
+#finally:
+manager.close()
+
--- utils/asterisk-connector/handlers/commands.py
+++ utils/asterisk-connector/handlers/commands.py
@@ -9,15 +9,19 @@ from channel.channel_message import ChannelMessage as ChannelMessage
 def handle_Dial(raw):
     return raw
 
-def handle_Hangup(event):
+def handle_Hangup(event, manager=None):
+    print event
+    print manager
+
     if event['Cause-txt'] == 'Normal Clearing':
-	return handle_hangup_clearing(event)
+        return handle_hangup_clearing(event)
 
-def handle_Link(event):
+def handle_Link(event, manager=None):
 
-    # Event: Link
+    print type(event)
+    print event
+
     # Original data: {'Uniqueid2': '1306914758.6999', 'Uniqueid1': '1306914726.6994', 'Channel1': 'SIP/430913-19be0080', 'Channel2': 'SIP/1313-19ba26d0', 'CallerID2': '380352407040', 'Privilege': 'call,all', 'CallerID1': '430913', 'Event': 'Link'}
-    # Produced message: {"t":"ln"}
 
     message = ChannelMessage()
 
@@ -37,9 +41,14 @@ def handle_Newchannel(raw):
 def handle_Newexten(raw):
     return raw
 
-def handle_Newstate(event):
-   if event['State'] == 'Ringing':
+def handle_Newstate(event, manager=None):
+    if manager != None:
+	event = event.headers
+   
+    if event['State'] == 'Ringing':
 	return handle_newstate_ringing(event)
+
+    return None
 
 def handle_Unlink(raw):
     return raw
