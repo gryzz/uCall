8f01c16db3a700e8df1842d04e9f13920a79aa47
rstets <romko.stets@gmail.com>
2011-06-29 22:51:01 +0300

    - Added feature detector

--- web/ui/bootstrap.js
+++ web/ui/bootstrap.js
@@ -1,9 +1,13 @@
 // Configuration
 Ext.Loader.setConfig({enabled: true});
 Ext.Loader.setPath('uCall', '/ui/app');
+Ext.Loader.setPath('Ext.env', '/ui/vendors/probonogeek/extjs/src/core/src/env');
+
 
 // Includes
 Ext.require('uCall.App');
+// TODO: requires are used with ext.js not ext-all.js - ext.js does not work as it has to - fix it.
+// TODO: try not to use wildcards
 Ext.require('Ext.direct.*');
 Ext.require('Ext.form.*');
 Ext.require('Ext.tip.QuickTipManager');
@@ -20,3 +24,8 @@ Ext.application({
         Ext.create('uCall.App');
     }
 });
+
+
+Ext.syncRequire('Ext.env.FeatureDetector');
+var featureDetector = Ext.create('Ext.env.FeatureDetector');
+console.log("Websockets supported: " + featureDetector.tests.Websockets());        
